e0d7c95d2c094f63fd99f424b7dffa4b706c7e4d
Teodora <tydi18@abv.bg>
2018-10-09 16:07:17 +0300

    Commited travel to sun

---
+++ PUIntroHomeworks/Travel2TheSunWithSound/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Travel2TheSunWithSound
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int distance2SunFromEarth = 149600000;
+            short speedOfSoundIn20DegreeLater = 1481;
+
+            double timeNeeded = (double)distance2SunFromEarth / speedOfSoundIn20DegreeLater;
+
+            Console.WriteLine($"Time needed:{timeNeeded}");
+
+        }
+    }
+}
