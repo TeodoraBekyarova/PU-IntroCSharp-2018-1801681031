f0ea4870ee30810a71e09255061def094ca21ea4
Teodora B <tydi18@abv.bg>
2018-10-13 22:22:47 +0300

    week2

---
+++ PUIntroHomeworks/WeightInTheSolarSystem/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace WeightInTheSolarSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double gravitationalRatio;
+            double weightOnPlanet;
+
+            Console.WriteLine("Enter your weight in kgs as measured on Earth:");
+            byte weight = 55;
+            Console.WriteLine(55);
+
+            Console.WriteLine("Tell me my weight in Mars");
+            ravitationalRatio = 0.38;
+            weightOnPlanet = gravitationalRatio * weight;
+            Console.WriteLine($"You weigh approximately {weightOnPlanet}kg on Mars.");
+        }
+    }
+}
